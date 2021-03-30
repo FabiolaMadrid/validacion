08ff43b6aa29eef71fbbabf2f21309f617ed352e
Fabiola Madrid <fabiola@empowerlabs.com>
2021-03-30 12:31:51 -0600

    Practica 3

--- pro/routes/web.php
+++ pro/routes/web.php
@@ -29,3 +29,7 @@ Route::delete('Lista/{matricula}','ListaController@destroy')
 
 Route::get('Lista/{estudiante}/edit', 'ListaController@edit')->name('Lista.edit');
 Route::patch('Lista/{estudiante}/edit', 'ListaController@update')->name('Lista.update');
+
+Route::get('grafica', function (){
+    return view('grafica');
+});
